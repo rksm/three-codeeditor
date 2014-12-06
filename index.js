13b836fea85a75f04f28442fab55135e324dfc11
Robert Krahn <robert.krahn@gmail.com>
2014-12-06 01:01:30 -0800

    scrollbar support

--- index.js
+++ index.js
@@ -36,6 +36,9 @@
       canvas3dElement.offsetLeft, canvas3dElement.offsetTop, width, height);
     aceEditor.renderer.on("afterRender",
       rendering.onAceEditorAfterRenderEvent.bind(null, aceEditor, this));
+    aceEditor.renderer.on("themeChange", this.invalidateScrollbar.bind(this));
+    aceEditor.renderer.on("resize", this.invalidateScrollbar.bind(this));
+    aceEditor.renderer.on("autosize", this.invalidateScrollbar.bind(this));
 
     // input event setup
     mouseevents.addMouseEventListener(THREExDOMEvents, this);
@@ -52,7 +55,29 @@
     this.setValue = function(text) {
       this.aceEditor.setValue(text);
     };
-  
+
+    this.toggleFocusAndBlurOnMouseOverAndOut = function() {
+      if (this._focusOnOver) {
+        var method = "removeEventListener";
+      } else {
+        method = "addEventListener";
+        this._focusOnOver = function(evt) {
+          var evts = THREE.CodeEditor.mouseevents;
+          if (!evts.isLeftMouseButtonPressed(evt) && !evts.isRightMouseButtonPressed(evt))
+            this.aceEditor.focus();
+        }.bind(this);
+        this._blurOnOut = function(evt) { this.aceEditor.blur(); }.bind(this);
+      }
+      this.THREExDOMEvents[method](this, "mouseover", this._focusOnOver, false);
+      this.THREExDOMEvents[method](this, "mouseout", this._blurOnOut, false);
+    }
+
+    this.getScrollbar = function() {
+      return this.scrollbar || (this.scrollbar = createScrollbar(this.aceEditor));
+    }
+
+    this.invalidateScrollbar = function(codeEditor) { codeEditor.scrollbar = null; }
+
     this.destroy = function() {
       // FIXME remove mouse handler...
       this.canvas2d.cleanup();
@@ -73,6 +98,30 @@
   function plane(a,b,c,d) {
     var vec1 = b.clone().sub(a), vec2 = d.clone().sub(a);
     return new THREE.PlaneGeometry(vec1.length(), vec2.length(), 10,10);
+  }
+
+  function createScrollbar(aceEditor) {
+    var renderer       = aceEditor.renderer, scrollBarV = renderer.scrollBarV,
+        editorStyle    = window.getComputedStyle(renderer.container),
+        relativeHeight = scrollBarV.element.clientHeight / scrollBarV.inner.clientHeight,
+        relativeTop    = scrollBarV.scrollTop / scrollBarV.inner.clientHeight,
+        height         = scrollBarV.element.clientHeight,
+        borderWidth    = 3,
+        width          = 10,
+        col            = new THREE.Color(editorStyle.backgroundColor),
+        isDarkTheme = (col.r+col.g+col.b)/3 < .5,
+        backgroundColor = col.clone().add(col.clone().multiplyScalar(-.4)).getStyle();
+
+    return {
+        height: height * relativeHeight - borderWidth,
+        width: width - borderWidth,
+        get top() { return height * (scrollBarV.scrollTop / scrollBarV.inner.clientHeight) + borderWidth; },
+        left: renderer.container.clientWidth - width - borderWidth,
+        borderWidth: borderWidth,
+        backgroundColor: backgroundColor,
+        borderColor: isDarkTheme ? col.add(new THREE.Color('white').multiplyScalar(.2)).getStyle() : backgroundColor,
+        borderRounding: 4
+      };
   }
 
 })(THREE, ace);
