5c98104587e5f71814868195f2332990aaa5a269
Robert Krahn <robert.krahn@gmail.com>
2014-12-10 18:54:37 -0800

    factoring events into more managable pieces, towards editor object

--- index.js
+++ index.js
@@ -3,70 +3,81 @@
 ;(function() {
 
     // "imports"
-  var aceHelper, rendering, canvas2d, domevents, mouseevents;
+  var aceHelper, rendering, canvas2d, domevents,
+      mouseevents, commands, htmlTHREEConversion;
+  // "imports" assigned here b/c they are first available after this module got defined
+  function imports() {
+    aceHelper           = THREE.CodeEditor.aceHelper,
+    rendering           = THREE.CodeEditor.rendering,
+    canvas2d            = THREE.CodeEditor.canvas2d,
+    mouseevents         = THREE.CodeEditor.mouseevents;
+    domevents           = THREE.CodeEditor.domevents;
+    commands            = THREE.CodeEditor.commands;
+    htmlTHREEConversion = THREE.CodeEditor["html-three-conversion"];
+  }
 
   // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
-  THREE.CodeEditor = function(canvas3dElement, THREExDOMEvents) {
+  if (!THREE.CodeEditor) {
+    THREE.CodeEditor = function CodeEditor() { this.initialize.apply(this, arguments); }
+    THREE.CodeEditor.prototype = Object.create(THREE.Mesh.prototype);
+  } else imports();
 
-    // "imports" assigned here b/c they are first available after this module got defined
-    aceHelper    = THREE.CodeEditor.aceHelper,
-    rendering    = THREE.CodeEditor.rendering,
-    canvas2d     = THREE.CodeEditor.canvas2d,
-    mouseevents  = THREE.CodeEditor.mouseevents;
-    domevents    = THREE.CodeEditor.domevents;
-
-    // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-
-    this.THREExDOMEvents = THREExDOMEvents;
-  
-    var width = 400, height = 400;
-    var editorGeo = plane(
-      new THREE.Vector3(-width/2, height/2,0),
-      new THREE.Vector3( width/2, height/2,0),
-      new THREE.Vector3( width/2,-height/2,0),
-      new THREE.Vector3(-width/2,-height/2,0));
-  
-    // building the html canvas that will be used as a texture
-    var canvas = this.canvas2d = canvas2d.create(width, height),
-        texture	= new THREE.Texture(canvas),
-        material= new THREE.MeshBasicMaterial({
-          color: "white", map: texture,
-          side: THREE.DoubleSide});
-
-    // var maxAnisotropy = renderer.getMaxAnisotropy();
-		// texture.anisotropy = 16;
-
-    THREE.Mesh.call(this, editorGeo, material);
-  
-    editorGeo.computeBoundingBox();
-    this.position.copy(editorGeo.boundingBox.center());
-  
-    // creating the ace editor instance that will work behind the scenes as our "model"
-    var aceEditor  = this.aceEditor = aceHelper.createAceEditor(
-      canvas3dElement.offsetLeft, canvas3dElement.offsetTop, width, height);
-    aceEditor.renderer.on("afterRender",
-      rendering.onAceEditorAfterRenderEvent.bind(null, aceEditor, this));
-    aceEditor.renderer.on("themeChange", this.invalidateScrollbar.bind(this));
-    aceEditor.renderer.on("resize", this.invalidateScrollbar.bind(this));
-    aceEditor.renderer.on("autosize", this.invalidateScrollbar.bind(this));
-
-    // input event setup
-    mouseevents.addMouseEventListener(THREExDOMEvents, this);
-    this.scrollSpeed = 1;
-
-    texture.needsUpdate	= true;
-  };
-  
-  THREE.CodeEditor.prototype = Object.create(THREE.Mesh.prototype);
-  
   (function() {
-  
+
     this.isCodeEditor3D = true;
 
     // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
     // initialize-release
     // -=-=-=-=-=-=-=-=-=-
+
+    this.initialize = function(canvas3dElement, THREExDOMEvents) {
+      imports();
+
+      this.THREExDOMEvents = THREExDOMEvents;
+    
+      var width = 400, height = 400;
+      var editorGeo = plane(
+        new THREE.Vector3(-width/2, height/2,0),
+        new THREE.Vector3( width/2, height/2,0),
+        new THREE.Vector3( width/2,-height/2,0),
+        new THREE.Vector3(-width/2,-height/2,0));
+    
+      // building the html canvas that will be used as a texture
+      var canvas = this.canvas2d = canvas2d.create(width, height),
+          texture	= new THREE.Texture(canvas),
+          material= new THREE.MeshBasicMaterial({
+            color: "white", map: texture,
+            side: THREE.DoubleSide});
+    
+      // var maxAnisotropy = renderer.getMaxAnisotropy();
+    	// texture.anisotropy = 16;
+    
+      THREE.Mesh.call(this, editorGeo, material);
+    
+      editorGeo.computeBoundingBox();
+      this.position.copy(editorGeo.boundingBox.center());
+    
+      // creating the ace editor instance that will work behind the scenes as our "model"
+      var aceEditor = this.aceEditor = aceHelper.createAceEditor(
+        canvas3dElement.offsetLeft, canvas3dElement.offsetTop, width, height);
+      var self = this;
+      aceEditor.renderer.on("afterRender", function() { rendering.onAceEditorAfterRenderEvent(aceEditor, self); });
+      aceEditor.renderer.on("themeChange", function() { self.invalidateScrollbar(); });
+      aceEditor.renderer.on("resize",      function() { self.invalidateScrollbar(); });
+      aceEditor.renderer.on("autosize",    function() { self.invalidateScrollbar(); });
+    
+      texture.needsUpdate	= true;
+    
+      this.addMouseEventListeners();
+    
+      // command setup
+      commands.javascript.forEach(function(cmd) {
+        aceEditor.commands.addCommand(cmd); });
+      var occurStartCommand = ace.require("ace/commands/occur_commands").occurStartCommand;
+      aceEditor.commands.addCommand(occurStartCommand);
+      aceEditor.setOption("useIncrementalSearch", true);
+    }
 
     this.destroy = function() {
       // FIXME remove mouse handler...
@@ -77,13 +88,13 @@
     };
 
     // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-    // editor behavior  
+    // editor behavior
     // -=-=-=-=-=-=-=-=-=-
 
     this.setValue = function(text) {
       this.aceEditor.setValue(text);
     };
-    
+
     this.insert = function(text, noTransform) {
       // insert text at cursor
       this.aceEditor.insert(text, noTransform);
@@ -92,6 +103,48 @@
     // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
     // input events
     // -=-=-=-=-=-=-
+
+    this.scrollSpeed = 1;
+    
+    this.addMouseEventListeners = function() {
+      mouseevents.patchTHREExDOMEventInstance(this.THREExDOMEvents);
+      if (this._onMouseDown || this._onMouseMove || this._onMouseWheel) this.removeMouseEventListeners();
+
+      this._onMouseDown  = function(evt) { return this.onMouseDown(evt); }.bind(this);
+      this._onMouseMove  = function(evt) { return this.onMouseMove(evt); }.bind(this);
+      this._onMouseWheel = function(evt) { return this.onMouseWheel(evt); }.bind(this);
+      this.THREExDOMEvents.addEventListener(this, 'mousedown', this._onMouseDown, false);
+      this.THREExDOMEvents.addEventListener(this, 'mousemove', this._onMouseMove, false);
+      this.THREExDOMEvents.addEventListener(this, 'mousewheel', this._onMouseWheel, false);
+    }
+
+    this.removeMouseEventListeners = function() {
+      this._onMouseDown &&  this.THREExDOMEvents.removeEventListener(this, 'mousedown', this._onMouseDown, false);
+      this._onMouseMove &&  this.THREExDOMEvents.removeEventListener(this, 'mousemove', this._onMouseMove, false);
+      this._onMouseWheel && this.THREExDOMEvents.removeEventListener(this, 'mousewheel', this._onMouseWheel, false);
+      this._onMouseDown = null;
+      this._onMouseMove = null;
+      this._onMouseWheel = null;
+    }
+
+    this.onMouseDown = function(evt) {
+      // clicked on scrollbar?
+      if (mouseevents.processScrollbarMouseEvent(
+          this.THREExDOMEvents, this, this.clickState, evt)) return true;
+
+      var aceCoords = htmlTHREEConversion.raycastIntersectionToDomXY(evt.intersect, this.aceEditor.container);
+      mouseevents.reemit3DMouseEvent(this.THREExDOMEvents, evt.origDomEvent, this.clickState, this, aceCoords);
+    }
+
+    this.onMouseMove = function(evt) {
+      var aceCoords = htmlTHREEConversion.raycastIntersectionToDomXY(evt.intersect, this.aceEditor.container);
+      mouseevents.reemit3DMouseEvent(this.THREExDOMEvents, evt.origDomEvent, this.clickState, this, aceCoords);
+    }
+
+    this.onMouseWheel = function(evt) {
+      var aceCoords = htmlTHREEConversion.raycastIntersectionToDomXY(evt.intersect, this.aceEditor.container);
+      mouseevents.reemit3DMouseEvent(this.THREExDOMEvents, evt.origDomEvent, this.clickState, this, aceCoords);
+    }
 
     this.toggleFocusAndBlurOnMouseOverAndOut = function() {
       if (this._focusOnOver) {
@@ -120,29 +173,40 @@
       doubleClickTriggerTime: 500,
       scrollbarClickPoint: null
     };
-    
-    this.onMouseDown = function(evt) {
-      // clicked on scrollbar?
-      if (mouseevents.processScrollbarMouseEvent(
-          this.THREExDOMEvents, this, this.clickState, evt)) return true;
-
-      var aceCoords = mouseevents.raycastIntersectionToDomXY(evt.intersect, this.aceEditor.container);
-      mouseevents.reemit3DMouseEvent(this.THREExDOMEvents, evt.origDomEvent, this.clickState, this, aceCoords);
-    }
-
-    this.onMouseMove = function(evt) {
-      var aceCoords = mouseevents.raycastIntersectionToDomXY(evt.intersect, this.aceEditor.container);
-      mouseevents.reemit3DMouseEvent(this.THREExDOMEvents, evt.origDomEvent, this.clickState, this, aceCoords);
-    }
-
-    this.onMouseWheel = function(evt) {
-      var aceCoords = mouseevents.raycastIntersectionToDomXY(evt.intersect, this.aceEditor.container);
-      mouseevents.reemit3DMouseEvent(this.THREExDOMEvents, evt.origDomEvent, this.clickState, this, aceCoords);
-    }
 
     // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
     // geometry
     // -=-=-=-=-
+
+    this.getWidth = function() { return this.getSize().x; };
+    this.getHeight = function() { return this.getSize().y; };
+
+    this.setHeight = function(height) { return this.setSize(this.getWidth(), height); };
+    this.setWidth = function(width) { return this.setSize(width, this.getHeight()); };
+
+    this.getSize = function() {
+      this.geometry.computeBoundingBox();
+      return this.geometry.boundingBox.size();
+    };
+
+    this.setSize = function(width, height) {
+      this.aceEditor.container.style.width = width + "px";
+      this.aceEditor.container.style.height = height + "px";
+      this.canvas2d.width = width;
+      this.canvas2d.height = height;
+      this.geometry.vertices = [
+        new THREE.Vector3(-width/2, height/2,0),
+        new THREE.Vector3( width/2, height/2,0),
+        new THREE.Vector3(-width/2,-height/2,0),
+        new THREE.Vector3( width/2,-height/2,0)]
+
+      this.material.map.needsUpdate = true;
+      this.aceEditor.resize(true);
+      this.geometry.verticesNeedUpdate = true;
+      // for events:
+      this.geometry.computeBoundingBox();
+      this.geometry.computeBoundingSphere();
+    };
 
     this.getGlobalVertice = function(i) {
       return this.geometry.vertices[i].clone().applyMatrix4(this.matrixWorld);
@@ -153,15 +217,54 @@
     this.bottomLeft = function() { return this.getGlobalVertice(2); };
     this.bottomRight = function() { return this.getGlobalVertice(3); };
 
+    // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+    // positioning
+    // -=-=-=-=-=-=-
+    this.alignWithCamera = function(leftRightOrCenter, camera) {
+      // offset: // -1 left, 0 center, 1 right
+      this.geometry.computeBoundingBox()
+      var margin = 50;
+      var size = this.geometry.boundingBox.size()
+      var dist = (size.y+margin) / 2 / Math.tan(Math.PI * camera.fov / 360);
+    	var center = htmlTHREEConversion.pickingRay({x:0,y:0}, camera).ray.at(dist);
+
+      this.position.copy(center);
+      this.lookAt(camera.position);
+      
+    // 	var projectionPoint = htmlTHREEConversion.pickingRay({x:-1,y:0}, camera).ray.at(dist);
+    // 	var delta = projectionPoint.clone().sub(center);
+    //   align(this, this.topLeft(), projectionPoint)
+    //   this.position.copy(projectionPoint.clone());
+    //   this.lookAt(camera.position.clone().add(delta));
+    //   this.position.add(this.topRight().clone().sub(this.topLeft()).multiplyScalar(.5));
+    //   return;
+
+      // move the editor to the left side until it reaches the screen border...
+      // ugly! I should use fov and stuff to compute the coordinates but its soooo late already.... :P
+      camera.updateMatrix()
+      camera.updateMatrixWorld()
+      var delta = camera.up.clone().cross(this.position.clone().sub(camera.position)).normalize()
+      var frustum = new THREE.Frustum();
+      frustum.setFromMatrix( new THREE.Matrix4().multiply( camera.projectionMatrix, camera.matrixWorldInverse ) );
+      var bounds = new THREE.Box3().setFromObject(this);
+      do { // move left unti we "hit" the corner of the editor
+        this.position.add(delta);
+        this.updateMatrixWorld();
+      } while (frustum.containsPoint(this.topLeft()) && frustum.containsPoint(this.bottomLeft()))
+
+    }
+
   }).call(THREE.CodeEditor.prototype);
-  
-  
+
+
   // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
   // helper
   // -=-=-=-
-  
+
+  function align(object, x, y) { return object.position.add(y.clone().sub(x)); }
+
   function notYetImplemented() { console.warn("NOT YET IMPLEMENTED"); }
-  
+
   function plane(a,b,c,d) {
     var vec1 = b.clone().sub(a), vec2 = d.clone().sub(a);
     return new THREE.PlaneGeometry(vec1.length(), vec2.length(), 1,1);
@@ -190,4 +293,4 @@
       };
   }
 
-})(THREE, ace);
+})(THREE);
