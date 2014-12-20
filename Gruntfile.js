10e89bcf90c8c71cd623018025157e763140becb
Robert Krahn <robert.krahn@gmail.com>
2014-12-19 23:13:02 -0800

    adding a grunt build task

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,7 @@ module.exports = function(grunt) {
     // js tests
     // -=-=-=-=-
     jshint: {
-      files: ['Gruntfile.js', 'lib/**/*.js', 'tests/**/*.js'],
+      files: ['Gruntfile.js', 'index.js', 'lib/**/*.js', 'tests/**/*.js'],
       options: {
         laxbreak: true,
         globals: {console: true, module: true, document: true}
@@ -36,18 +36,47 @@ module.exports = function(grunt) {
                + 'rm -rf ace-builds-master master.tar.gz',
         options: {execOptions: {cwd: 'vendor/ace/'}}
       },
-      updateTHREE: {
-        command: 'mkdir -p examples/three/; mv vendor/three/{ColladaLoader,OrbitControls,VREffect,VRControls}.js examples/three/'
-      },
       runTests: {
         command: '<%= pkg.scripts.test %>',
         options: {execOptions: {}}
       }
+    },
+
+    // -=-=-=-=-=-=-=-
+    // build bundles
+    // -=-=-=-=-=-=-=-
+    concat: {
+      options: {sourceMap: true, sourceMapStyle: 'link', separator: ';\n'},
+      "codeeditor3d.dev-bundle.js": {
+        src: ["vendor/ace/src-noconflict/ace.js",
+              "index.js",
+              "lib/ace-helper.js",
+              "lib/canvas2d.js",
+              "lib/domevents.js",
+              "lib/raycasting.js",
+              "lib/mouseevents.js",
+              "lib/rendering.js",
+              "lib/autocomplete.js"],
+        dest: "codeeditor3d.dev-bundle.js"
+      }
+    },
+
+    uglify: {
+      "codeeditor3d.min-bundle.js": {
+        options: {
+          sourceMap: true,
+          banner: '/*! <%= pkg.name %>-v<%= pkg.version %> '
+                + '<%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        },
+        files: {"codeeditor3d.min-bundle.js": "codeeditor3d.dev-bundle.js"}
+      }
     }
+
   });
 
-  grunt.registerTask('test', ['jshint', 'mochaTest']);
+  grunt.registerTask('test', ['jshint', 'shell:runTests']);
   grunt.registerTask('updateAce', ['curl-dir:updateAce', 'shell:updateAce']);
-  grunt.registerTask('updateTHREE', ['curl-dir:updateTHREE', 'shell:updateTHREE']);
-  grunt.registerTask('updateLibs', ['updateAce', 'updateTHREE']);
+  grunt.registerTask('updateLibs', ['updateAce']);
+  grunt.registerTask('build', ['concat:codeeditor3d.dev-bundle.js']);
+  
 };
